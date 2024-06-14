61bb5aebf40628d4ea47ca6c9c8a11ac3dd7d00c
NathanBarisic <nathan.barisic@htl-wels.at>
2024-06-14 08:56:05 +0200

    Nathan (3rd)

--- src/ApplicationServer.java
+++ src/ApplicationServer.java
@@ -7,6 +7,7 @@ Beschreibung: Umsetzung des Tankkarten Projekts
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Date;
 
 public class ApplicationServer {
     private static final int PORT = 80;
@@ -18,11 +19,47 @@ public class ApplicationServer {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
 
-                //Thread t = new Thread(new Clienthandler(clientSocket));
+                //Thread t = new Thread();
                 //t.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    //Die PAN der Tankkarte wird auf gültigkeit mittels dem Luhn Algorithmus geprüft
+    public boolean pruefeTankkarte(long pan) {
+        String cardNo = Long.toString(pan);  // Konvertiert die PAN zu einem String, um die Ziffern zu durchlaufen
+        int nDigits = cardNo.length();
+        int nSum = 0;
+        boolean isSecond = false;
+
+        // Durchläuft die Ziffern der PAN von rechts nach links
+        for (int i = nDigits - 1; i >= 0; i--) {
+            int d = cardNo.charAt(i) - '0';  // Konvertiert den Zeichen zu einer Zahl
+
+            if (isSecond) {
+                d = d * 2;
+            }
+
+            // Fügt die Ziffern der Zahl hinzu, wenn sie größer als 9 ist, wird sie in ihre Bestandteile zerlegt (z.B. 18 => 1 + 8)
+            nSum += d / 10;
+            nSum += d % 10;
+
+            isSecond = !isSecond;  // Wechselt isSecond für die nächste Ziffer
+        }
+
+        return (nSum % 10 == 0); //Überprüfung auf Vielfaches von 10
+    }
+
+
+    public boolean saveTransaktion(Transaktion t){
+
+        return true;
+    }
+
+    public void calcMonatsabrechnung(Date startDate, Date endDate){
+
     }
 }
